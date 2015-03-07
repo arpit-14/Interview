02fd9ee012961b45a960a8e814aea91bfa5c4bbf
Arpit Gupta <arpitgup@10ddb1d55079.ant.amazon.com>
2015-03-07 17:04:16 +0530

    First commit
    first commit

---
+++ src/com/arpit/hackerearth/ChessTournament.java
@@ -0,0 +1,55 @@
+// http://www.hackerearth.com/problem/algorithm/chess-tournament-4/
+
+package com.arpit.hackerearth;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.Math;
+import java.util.*;
+ 
+class ChessTournament {
+    public static void main(String args[] ) throws Exception {
+ 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        int N = Integer.parseInt(line);
+        
+        int total = (int) Math.pow(2, (double)N);
+        
+        int[][] players = new int[total + 1][total + 1];
+        
+        int count = 2;
+        for (int i = 2; i<=total; i++) {
+        	String[] input = br.readLine().split(" ");
+            for(int j=1; j<i; j++){
+            	players[i][j] = Integer.parseInt(input[j-1]);	
+            }
+        }
+ 
+		LinkedList<Integer> activePlayers = new LinkedList<Integer>();
+		for(int i=1; i<=total; i++){
+			activePlayers.add(new Integer(i));
+		}
+ 
+		count = total;
+ 
+		while(count>1){
+			
+			for(int i=0; i<count-1; i= i+2){
+				if(players[activePlayers.get(i+1)][activePlayers.get(i)] == 1){
+					activePlayers.set(i, -1);
+				}else{
+					activePlayers.set(i+1, -1);
+				}
+			}
+			
+			while(activePlayers.removeFirstOccurrence(-1)){
+				
+			}
+			count = activePlayers.size();
+		}
+        
+        System.out.println(activePlayers.getFirst());
+    }
+}
+
---
+++ src/com/arpit/leetcode/ValidParentheses.java
@@ -0,0 +1,65 @@
+// https://oj.leetcode.com/problems/valid-parentheses/
+
+package com.arpit.leetcode;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        
+        if((null == s) || (s.trim() == "") ){
+            return true;
+        }
+        
+        char open1 = '(';
+        char close1 = ')';
+        char open2 = '{';
+        char close2 = '}';
+        char open3 = '[';
+        char close3 = ']';
+        
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+        
+        int lastOpen = 0;
+        
+        int[] input = new int[s.length()];
+        int j = -1;
+        
+        for(int i=0; i<s.length(); i++){
+            
+            char temp = s.charAt(i);
+            if( temp == open1){
+                input[++j] = 1;
+                count1++; 
+            }else if( temp == open2){
+                input[++j] = 2;
+                count2++; 
+            }else if( temp == open3){
+                input[++j] = 3;
+                count3++; 
+            }else if( temp == close1 ){
+                if( j<0 || input[j--] != 1){
+                    return false;
+                }
+                count1--;
+            }else if(  temp == close2 ){
+                if( j<0 ||  input[j--] != 2){
+                    return false;
+                }
+                count2--;
+            }else if(  temp == close3 ){
+                if( j<0 || input[j--] != 3){
+                    return false;
+                }
+                count3--;
+            }
+        }
+        
+        if((count1 == 0) && (count2 == 0) && (count3 == 0) ){
+            return true;
+        }
+        
+        return false;
+        
+    }
+}
